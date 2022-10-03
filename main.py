ca6b32b0ce3e63bb7004f33616ab6bb607fa131e
Yuchen Xie <38805981+Sika-R@users.noreply.github.com>
2022-10-02 20:39:43 -0400

    Return accelerometer directions

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 x = 0
 y = 0
 length = 0
-def ParseAccelerometer():
+class MicrobitDirection(Enum):
+    Null = 0
+    Up = 1
+    Down = 2
+    Left = 3
+    Right = 4
+def parse_accelerometer():
     global x, y, length
+    direction = MicrobitDirection.Null
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
     length = y * y + x * x
@@ -19,22 +26,32 @@ def ParseAccelerometer():
             outer.append(4)
             inner.append(2)
             inner.append(3)
+            direction = MicrobitDirection.Right
         elif deg < 135:
             outer.append(0)
             outer.append(2)
             inner.append(1)
             inner.append(2)
+            direction = MicrobitDirection.Up
         elif deg < 225:
             outer.append(2)
             outer.append(0)
             inner.append(2)
             inner.append(1)
+            direction = MicrobitDirection.Left
         elif deg <= 315:
             outer.append(4)
             outer.append(2)
             inner.append(3)
             inner.append(2)
+            direction = MicrobitDirection.Down
         led.plot(inner[1], inner[0])
         if length > 80000:
             led.plot(outer[1], outer[0])
     led.plot(2, 2)
+    return direction
+
+while True:
+    parse_accelerometer()
+    basic.pause(100)
+    basic.clear_screen()
--- main.ts
+++ main.ts
@@ -1,11 +1,80 @@
 let x = 0
 let y = 0
 let length = 0
-function ParseAccelerometer() {
+class MicrobitDirection {
+    static Null: number
+    private ___Null_is_set: boolean
+    private ___Null: number
+    get Null(): number {
+        return this.___Null_is_set ? this.___Null : MicrobitDirection.Null
+    }
+    set Null(value: number) {
+        this.___Null_is_set = true
+        this.___Null = value
+    }
+    
+    static Up: number
+    private ___Up_is_set: boolean
+    private ___Up: number
+    get Up(): number {
+        return this.___Up_is_set ? this.___Up : MicrobitDirection.Up
+    }
+    set Up(value: number) {
+        this.___Up_is_set = true
+        this.___Up = value
+    }
+    
+    static Down: number
+    private ___Down_is_set: boolean
+    private ___Down: number
+    get Down(): number {
+        return this.___Down_is_set ? this.___Down : MicrobitDirection.Down
+    }
+    set Down(value: number) {
+        this.___Down_is_set = true
+        this.___Down = value
+    }
+    
+    static Left: number
+    private ___Left_is_set: boolean
+    private ___Left: number
+    get Left(): number {
+        return this.___Left_is_set ? this.___Left : MicrobitDirection.Left
+    }
+    set Left(value: number) {
+        this.___Left_is_set = true
+        this.___Left = value
+    }
+    
+    static Right: number
+    private ___Right_is_set: boolean
+    private ___Right: number
+    get Right(): number {
+        return this.___Right_is_set ? this.___Right : MicrobitDirection.Right
+    }
+    set Right(value: number) {
+        this.___Right_is_set = true
+        this.___Right = value
+    }
+    
+    public static __initMicrobitDirection() {
+        MicrobitDirection.Null = 0
+        MicrobitDirection.Up = 1
+        MicrobitDirection.Down = 2
+        MicrobitDirection.Left = 3
+        MicrobitDirection.Right = 4
+    }
+    
+}
+
+MicrobitDirection.__initMicrobitDirection()
+
+function parse_accelerometer(): number {
     let deg: number;
     let inner: number[];
     let outer: number[];
     
+    let direction = MicrobitDirection.Null
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
     length = y * y + x * x
@@ -24,21 +93,25 @@ function ParseAccelerometer() {
             outer.push(4)
             inner.push(2)
             inner.push(3)
+            direction = MicrobitDirection.Right
         } else if (deg < 135) {
             outer.push(0)
             outer.push(2)
             inner.push(1)
             inner.push(2)
+            direction = MicrobitDirection.Up
         } else if (deg < 225) {
             outer.push(2)
             outer.push(0)
             inner.push(2)
             inner.push(1)
+            direction = MicrobitDirection.Left
         } else if (deg <= 315) {
             outer.push(4)
             outer.push(2)
             inner.push(3)
             inner.push(2)
+            direction = MicrobitDirection.Down
         }
         
         led.plot(inner[1], inner[0])
@@ -49,5 +122,11 @@ function ParseAccelerometer() {
     }
     
     led.plot(2, 2)
+    return direction
 }
 
+while (true) {
+    parse_accelerometer()
+    basic.pause(100)
+    basic.clearScreen()
+}
